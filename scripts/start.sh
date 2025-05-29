0d793f55896ea909d87982ee702010a5e966d514
Shankar M <msshankar240698@gmail.com>
2025-05-29 19:24:03 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run -d -p 3000:3000 --name nodejs-container thanosda/nodejs-docker-app:latest
+docker run -d -p 3000:3000 \
+  -e NEW_RELIC_NO_CONFIG_FILE=true \
+  -e NEW_RELIC_LICENSE_KEY=844ef824bcd64aebc55cbbba52db8171FFFFNRAL \
+  -e NEW_RELIC_APP_NAME="MyNodeApp" \
+  -e NEW_RELIC_DISTRIBUTED_TRACING_ENABLED=true \
+  -e NEW_RELIC_LOG_LEVEL=info \
+  --name nodejs-container \
+  thanosda/nodejs-docker-app:latest
